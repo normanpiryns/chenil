9cf1465069929af210a64eae0b0850928bc0f4db
Emile Cyimena <cyimena09@hotmail.com>
2021-05-13 18:06:45 +0200

    project structure dev

--- models/dao/RaceDao.php
+++ models/dao/RaceDao.php
@@ -1,11 +1,78 @@
 <?php
 
-class RaceDao extends AbstractDao {
+class RaceDao extends AbstractDao
+{
 
     public function __construct()
     {
-        //appelle le constructeur du parent (AbstractDAO)
+        // call parent constructor (AbstractDAO)
         parent::__construct('races');
+    }
+
+    public function getRaces()
+    {
+
+    }
+
+    public function getRaceById($id)
+    {
+
+    }
+
+    public function addRace($race)
+    {
+        if(empty($data['name']) || empty($data['image']) || empty($data['pokemon_id'])) {
+            return false;
+        }
+
+        $pokemon = $this->create(
+            [
+                'id'=> 0,
+                'name'=> $data['name'],
+                'image' => $data['image'],
+                'pokemon_id'=> $data['pokemon_id'],
+                'user_id' => $data['user_id']
+            ]
+        );
+
+        if ($pokemon) {
+            try {
+                $statement = $this->connection->prepare(
+                    "INSERT INTO {$this->table} (name, image, pokemon_id, user_id) VALUES (?, ?, ?, ?)"
+                );
+                $statement->execute([
+                    htmlspecialchars($pokemon->__get('name')),
+                    htmlspecialchars($pokemon->__get('image')),
+                    htmlspecialchars($pokemon->__get('pokemon_id')),
+                    htmlspecialchars($pokemon->__get('user'))
+                ]);
+                return true;
+            } catch(PDOException $e) {
+                print $e->getMessage();
+                return false;
+            }
+        }
+    }
+
+    public function updateRace($race)
+    {
+
+    }
+
+    public function deleteRace($id)
+    {
+        if(empty($data['id'])) {
+            return false;
+        }
+
+        try {
+            $statement = $this->connection->prepare("DELETE FROM {$this->table} WHERE id = ?");
+            $statement->execute([
+                $data['id']
+            ]);
+        } catch(PDOException $e) {
+            print $e->getMessage();
+        }
     }
 
 }
--- models/dao/VaccineDao.php
+++ models/dao/VaccineDao.php
@@ -4,8 +4,73 @@ class VaccineDao extends AbstractDao {
 
     public function __construct()
     {
-        //appelle le constructeur du parent (AbstractDAO)
+        // call parent constructor (AbstractDAO)
         parent::__construct('vaccines');
     }
 
+    public function getVaccines()
+    {
+
+    }
+
+    public function getVaccineById($id)
+    {
+
+    }
+
+    public function addVaccine($vaccine)
+    {
+        if(empty($data['name']) || empty($data['image']) || empty($data['pokemon_id'])) {
+            return false;
+        }
+
+        $pokemon = $this->create(
+            [
+                'id'=> 0,
+                'name'=> $data['name'],
+                'image' => $data['image'],
+                'pokemon_id'=> $data['pokemon_id'],
+                'user_id' => $data['user_id']
+            ]
+        );
+
+        if ($pokemon) {
+            try {
+                $statement = $this->connection->prepare(
+                    "INSERT INTO {$this->table} (name, image, pokemon_id, user_id) VALUES (?, ?, ?, ?)"
+                );
+                $statement->execute([
+                    htmlspecialchars($pokemon->__get('name')),
+                    htmlspecialchars($pokemon->__get('image')),
+                    htmlspecialchars($pokemon->__get('pokemon_id')),
+                    htmlspecialchars($pokemon->__get('user'))
+                ]);
+                return true;
+            } catch(PDOException $e) {
+                print $e->getMessage();
+                return false;
+            }
+        }
+    }
+
+    public function updateVaccine($vaccine)
+    {
+
+    }
+
+    public function deleteVaccine($id)
+    {
+        if(empty($data['id'])) {
+            return false;
+        }
+
+        try {
+            $statement = $this->connection->prepare("DELETE FROM {$this->table} WHERE id = ?");
+            $statement->execute([
+                $data['id']
+            ]);
+        } catch(PDOException $e) {
+            print $e->getMessage();
+        }
+    }
 }
