5dd2e6aa4bb89a4bc2a0472a56e0dff24397e6e1
Emile Cyimena <cyimena09@hotmail.com>
2021-05-16 13:37:59 +0200

    add frontend template

--- views/vaccines/create_form.php
+++ views/vaccines/create_form.php
@@ -1,20 +1,61 @@
-<div>
+<!--<div>-->
+<!---->
+<!--    <h2>Ajouter un nouveau vaccin</h2>-->
+<!---->
+<!--    <form  action="/vaccines/create" method="POST">-->
+<!---->
+<!--        <div>-->
+<!--            <label for="name">Nom</label>-->
+<!--            <input id="name" type="text" name="name"/>-->
+<!--        </div>-->
+<!---->
+<!--        <div>-->
+<!--            <label for="description">Description</label>-->
+<!--            <input id="description" type="text" name="description"/>-->
+<!--        </div>-->
+<!---->
+<!--        <input type="submit" value="Enregistrer" />-->
+<!--    </form>-->
+<!---->
+<!--</div>-->
 
-    <h2>Ajouter un nouveau vaccin</h2>
+<div class="single-person-animal page">
+    <div class="content-page">
+        <h1 style="margin-bottom: 20px">Ajouter un maître</h1>
+        <!--    Animal-->
+        <h2>Information du maître</h2>
+        <div class="animal-cart-info">
 
-    <form  action="/vaccines/create" method="POST">
 
-        <div>
-            <label for="name">Nom</label>
-            <input id="name" type="text" name="name"/>
+            <form action="">
+                <div class="group">
+                    <label class="label" for="name">Prénom</label>
+                    <input id="name" type="text" value="Rodger">
+                </div>
+
+                <div class="group">
+                    <label class="label" for="sex">Nom</label>
+                    <input id="sex" type="text" value="Rodger">
+                </div>
+
+                <div class="group">
+                    <label class="label" for="birthDate">Date de naissance</label>
+                    <input id="birthDate" type="text" value="Rodger">
+                </div>
+
+                <div class="group">
+                    <label class="label" for="race">Email</label>
+                    <input id="race" type="text" value="Rodger">
+                </div>
+
+                <div class="group">
+                    <label class="label" for="sterilized">Téléphone</label>
+                    <input id="sterilized" type="text" value="Rodger">
+                </div>
+
+                <input type="submit" value="Créer le maître">
+            </form>
+
         </div>
-
-        <div>
-            <label for="description">Description</label>
-            <input id="description" type="text" name="description"/>
-        </div>
-
-        <input type="submit" value="Enregistrer" />
-    </form>
-
+    </div>
 </div>
--- views/vaccines/one.php
+++ views/vaccines/one.php
@@ -1,16 +1,16 @@
 
-<form action="/vaccines/update/<?= $vaccine->id ?>" method="POST">
-
-    <div>
-        <label for="name">Nom</label>
-        <input type="text" name="name" id="name" value="<?= $vaccine->name ?>">
-    </div>
-
-    <div>
-        <label for="description">Description</label>
-        <input type="text" name="description" id="description" value="<?= $vaccine->description ?>">
-    </div>
-
-
-    <input type="submit" value="Envoyer">
-</form>
+<!--<form action="/vaccines/update/--><?//= $vaccine->id ?><!--" method="POST">-->
+<!---->
+<!--    <div>-->
+<!--        <label for="name">Nom</label>-->
+<!--        <input type="text" name="name" id="name" value="--><?//= $vaccine->name ?><!--">-->
+<!--    </div>-->
+<!---->
+<!--    <div>-->
+<!--        <label for="description">Description</label>-->
+<!--        <input type="text" name="description" id="description" value="--><?//= $vaccine->description ?><!--">-->
+<!--    </div>-->
+<!---->
+<!---->
+<!--    <input type="submit" value="Envoyer">-->
+<!--</form>-->
